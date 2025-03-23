2a37871a9b86801b6f1e9fbf36a1877de0a3ba6d
ndmichael <ukejemicheal@gmail.com>
2025-03-23 18:53:48 +0100

    Layout adjusted for different screens

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -6,20 +6,40 @@ import Listings from './pages/Listings';
 import Buy from './pages/Buy';
 import Rent from './pages/Rent';
 
+// auth routes import
+import Signup from './pages/Auth/Signup'
+import Login from './pages/Auth/Login'
+
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import PropertyDetail from './pages/PropertyDetail';
+import AgentDashboard from './pages/agents/AgentDashboard';
+import LayoutWrapper from './layouts/LayoutWrapper';
+import MyListings from './pages/agents/MyListings';
+import AddProperty from './pages/agents/AddProperty';
+import EditProperty from './pages/agents/EditProperty';
 
 function App() {
 
   return (
     <Router>
-      <Navbar />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/listings" element={<Listings />} />
-        <Route path="/buy" element={<Buy />} />
-        <Route path="/rent" element={<Rent />} />
-      </Routes>
-      <Footer />
+      <LayoutWrapper>
+        <Routes>
+          {/* Public Routes */}
+          <Route path="/" element={<Home />} />
+          <Route path="/listings" element={<Listings />} />
+          <Route path="/property/:id" element={<PropertyDetail />} />
+          <Route path="/buy" element={<Buy />} />
+          <Route path="/rent" element={<Rent />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/signup" element={<Signup />} />
+
+          {/* Agent Routes */}
+          <Route path="/agent/dashboard" element={<AgentDashboard />} />
+          <Route path="/agent/mylistings" element={<MyListings />} />
+          <Route path="/agent/property/add" element={<AddProperty />} />
+          <Route path="/agent/property/edit" element={<EditProperty />} />
+        </Routes>
+      </LayoutWrapper>
     </Router>
   )
 }
