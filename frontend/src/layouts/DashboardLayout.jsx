23ae2e23325c4b1020dad7e0f465b4000deff622
ndmichael <ukejemicheal@gmail.com>
2025-03-24 12:38:38 +0100

    dashboardlayout adjusted

--- frontend/src/layouts/DashboardLayout.jsx
+++ frontend/src/layouts/DashboardLayout.jsx
@@ -6,7 +6,7 @@ const DashboardLayout = ({ children, role, title }) => {
   return (
     <Box sx={{ display: "flex", minHeight: "100vh" }}>
       <Sidebar role={role} />
-      <Box sx={{ flexGrow: 1, display: "flex", flexDirection: "column" }}>
+      <Box sx={{ flexGrow: 1, display: "flex", flexDirection: "column", minWidth: 0, }}>
         <CustomAppBar title={title} />
         <Box sx={{ p: 3 }}>{children}</Box>
       </Box>
