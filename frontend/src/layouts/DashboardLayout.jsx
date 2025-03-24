e1fa066836b1b6fe8b80addbf085a5be7f043bd9
ndmichael <ukejemicheal@gmail.com>
2025-03-24 13:14:38 +0100

    Made it full width

--- frontend/src/layouts/DashboardLayout.jsx
+++ frontend/src/layouts/DashboardLayout.jsx
@@ -6,9 +6,9 @@ const DashboardLayout = ({ children, role, title }) => {
   return (
     <Box sx={{ display: "flex", minHeight: "100vh" }}>
       <Sidebar role={role} />
-      <Box sx={{ flexGrow: 1, display: "flex", flexDirection: "column", minWidth: 0, }}>
+      <Box sx={{ flexGrow: 1, display: "flex", flexDirection: "column", minWidth: 0, width: "100%" }}>
         <CustomAppBar title={title} />
-        <Box sx={{ p: 3 }}>{children}</Box>
+        <Box py={3} sx={{ width: "100%" }}>{children}</Box>
       </Box>
     </Box>
   );
