e7a7ae144cd496c80995141157f959ce87c72d62
ndmichael <ukejemicheal@gmail.com>
2025-03-25 01:56:58 +0100

    user model updated

---
+++ backend/src/models/User.js
@@ -0,0 +1,32 @@
+const mongoose = require("mongoose");
+
+const UserSchema = new mongoose.Schema(
+  {
+    firstName: { type: String, required: true },
+    lastName: { type: String, required: true },
+    email: { type: String, required: true, unique: true },
+    password: { type: String, required: true },
+    phone: { type: String, required: true },
+    role: { type: String, enum: ["client", "agent", "admin"], required: true },
+    profileImage: { type: String }, // Cloudinary URL or File Path
+    isVerified: { type: Boolean, default: false }, // Admin verifies agents
+
+    savedListings: [{ type: mongoose.Schema.Types.ObjectId, ref: "Property" }], // Wishlist for Clients
+
+    agentDetails: {
+      companyName: { type: String },
+      licenseNumber: { type: String },
+    },
+
+    // Subscription Data
+    subscription: { type: mongoose.Schema.Types.ObjectId, ref: "Subscription" },
+    subscriptionExpiresAt: { type: Date, default: null },
+
+    //  Track Free Listings for Free Plan Agents
+    freeListingsRemaining: { type: Number, default: 5 }, // Resets Monthly
+    lastFreeListingReset: { type: Date, default: null }, // Tracks Last Reset
+  },
+  { timestamps: true }
+);
+
+module.exports = mongoose.model("User", UserSchema);
