315366452dfe0812cd10dfffd5eff6d34082336e
ndmichael <ukejemicheal@gmail.com>
2025-03-27 14:35:22 +0100

    Property routes for update and delete

--- backend/src/routes/propertyRoutes.js
+++ backend/src/routes/propertyRoutes.js
@@ -1,11 +1,19 @@
 import express from "express";
-import { createProperty, getAllProperties, getMyProperties } from "../controllers/propertyController.js";
+import { 
+    createProperty, 
+    getAllProperties, 
+    getMyProperties, 
+    updateProperty,
+    deleteProperty 
+} from "../controllers/propertyController.js";
 import { protect, authorize } from "../middlewares/authMiddlewares.js";
 
 const router = express.Router();
 
 router.post("/", protect, authorize("agent"), createProperty); // Only agents can create properties
-router.get("/", protect, getAllProperties); // Agents see their own listings, others see all
+router.get("/", getAllProperties); // Agents see their own listings, others see all
 router.get("/my-listings", protect, authorize("agent"), getMyProperties);
+router.put("/:id", protect, authorize("agent"), updateProperty); // Update property
+router.delete("/:id", protect, authorize("agent"), deleteProperty); // Delete property
 
 export default router;
