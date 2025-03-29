335ad1144ef3cd51f0886395ff2c454c67053362
ndmichael <ukejemicheal@gmail.com>
2025-03-29 21:04:35 +0100

    private routes and authentications

---
+++ frontend/src/routes/PrivateRoute.jsx
@@ -0,0 +1,17 @@
+import { Navigate, Outlet } from "react-router-dom";
+import { useSelector } from "react-redux";
+
+const PrivateRoute = ({ allowedRoles }) => {
+  const { user } = useSelector((state) => state.auth);
+  console.log(user)
+
+  if (!user) return <Navigate to="/login" replace />; // Redirect if not logged in
+  if (!allowedRoles.includes(user.user.role)) return <Navigate to="/unauthorized" replace />; // Restrict access
+
+  return <Outlet />;
+};
+
+export default PrivateRoute;
+
+
+
