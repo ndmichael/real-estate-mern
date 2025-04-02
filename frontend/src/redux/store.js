79eb7b0dbc9ad770cc35b15e377cb7929ceaaf61
ndmichael <ukejemicheal@gmail.com>
2025-04-02 11:48:28 +0100

    include user to the store

--- frontend/src/redux/store.js
+++ frontend/src/redux/store.js
@@ -1,11 +1,13 @@
 import { configureStore } from "@reduxjs/toolkit";
 import authReducer from "./authSlice";
 import propertyReducer from "./propertySlice";
+import userSlice from "./userSlice";
 
 const store = configureStore({
   reducer: {
     auth: authReducer,
-    property: propertyReducer
+    property: propertyReducer,
+    user: userSlice
   },
 });
 
