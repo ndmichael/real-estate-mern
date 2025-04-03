27a02b7f591668e498caf0c54ba9175a119effc6
ndmichael <ukejemicheal@gmail.com>
2025-04-03 13:16:03 +0100

    remove user from store

--- frontend/src/redux/store.js
+++ frontend/src/redux/store.js
@@ -1,13 +1,12 @@
 import { configureStore } from "@reduxjs/toolkit";
 import authReducer from "./authSlice";
 import propertyReducer from "./propertySlice";
-import userSlice from "./userSlice";
+
 
 const store = configureStore({
   reducer: {
     auth: authReducer,
     property: propertyReducer,
-    user: userSlice
   },
 });
 
