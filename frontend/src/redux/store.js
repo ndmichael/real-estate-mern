8cac78f4ff31e1fcf1ff67f331e4dd46a290f1ab
ndmichael <ukejemicheal@gmail.com>
2025-04-05 20:51:32 +0100

    Add agent to store

--- frontend/src/redux/store.js
+++ frontend/src/redux/store.js
@@ -1,12 +1,14 @@
 import { configureStore } from "@reduxjs/toolkit";
 import authReducer from "./authSlice";
 import propertyReducer from "./propertySlice";
+import agentReducer from './agentSlice';
 
 
 const store = configureStore({
   reducer: {
     auth: authReducer,
     property: propertyReducer,
+    agent: agentReducer,
   },
 });
 
