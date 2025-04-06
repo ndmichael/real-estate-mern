6fd2e9db636a20a0645f462cc6bda2297f6ea44e
ndmichael <ukejemicheal@gmail.com>
2025-04-06 14:43:09 +0100

    verified users

--- backend/src/routes/userRoutes.js
+++ backend/src/routes/userRoutes.js
@@ -1,10 +1,11 @@
 import express from "express";
-import { getUserProfile, updateUserProfile } from "../controllers/userController.js";
+import { getUserProfile, updateUserProfile, getVerifiedAgents } from "../controllers/userController.js";
 import { protect, authorize } from "../middlewares/authMiddlewares.js";
 
 const router = express.Router();
 
 router.get("/profile", protect, getUserProfile);
 router.put("/profile", protect, updateUserProfile);
+router.get("/agents", getVerifiedAgents)
 
 export default router;
