a8f4538fb9ab9d6ea004da142561dcaa72759654
ndmichael <ukejemicheal@gmail.com>
2025-04-06 14:43:33 +0100

    logic to also be able to add and remove images

--- backend/src/routes/propertyRoutes.js
+++ backend/src/routes/propertyRoutes.js
@@ -15,7 +15,7 @@ const router = express.Router();
 router.post("/", protect, authorize("agent"), upload.array('images', 4), createProperty); // Only agents can create properties
 router.get("/", getAllProperties); // Agents see their own listings, others see all
 router.get("/my-listings", protect, authorize("agent"), getMyProperties);
-router.put("/:id", protect, authorize("agent"), updateProperty); // Update property
+router.put("/:id", protect, authorize("agent"), upload.array("images", 4), updateProperty); // Update property
 router.delete("/:id", protect, authorize("agent"), deleteProperty); // Delete property
 router.get("/:id", getPropertyById);
 
