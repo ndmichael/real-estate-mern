483956ababbd7e2292954a231dbf307b0b901d84
ndmichael <ukejemicheal@gmail.com>
2025-04-07 05:30:50 +0100

    Router to user update form

--- backend/src/routes/userRoutes.js
+++ backend/src/routes/userRoutes.js
@@ -1,11 +1,12 @@
 import express from "express";
 import { getUserProfile, updateUserProfile, getVerifiedAgents } from "../controllers/userController.js";
 import { protect, authorize } from "../middlewares/authMiddlewares.js";
+import upload from "../middlewares/uploadMiddleware.js"
 
 const router = express.Router();
 
 router.get("/profile", protect, getUserProfile);
-router.put("/profile", protect, updateUserProfile);
+router.put("/profile", protect, upload.single('profileImage'),  updateUserProfile);
 router.get("/agents", getVerifiedAgents)
 
 export default router;
