8621cd74b23133e307bd1c4aa11f3e0a70e1d845
ndmichael <ukejemicheal@gmail.com>
2025-04-09 17:39:50 +0100

    Inquiry model updated

--- backend/src/models/Inquiry.js
+++ backend/src/models/Inquiry.js
@@ -2,15 +2,23 @@ import mongoose from "mongoose";
 
 const InquirySchema = new mongoose.Schema(
     {
-      property: { type: mongoose.Schema.Types.ObjectId, ref: "Property", required: true },
+      property: { type: mongoose.Schema.Types.ObjectId, ref: "Property", required: false },
       client: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true },
       agent: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true },
       message: { type: String, required: true },
+      reply: {
+        type: String,
+        default: "",
+      },
+      repliedAt: {
+        type: Date,
+        default: null,
+      },
       status: { type: String, enum: ["pending", "replied"], default: "pending" },
     },
     { timestamps: true }
   );
   
-  const Inquiry = mongoose.model("Subscription", InquirySchema);
+  const Inquiry = mongoose.model("Inquiry", InquirySchema);
   export default Inquiry;
   
