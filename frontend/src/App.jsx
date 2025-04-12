74e668f60a9cf70e446b42446ca2cee7cfac518c
ndmichael <ukejemicheal@gmail.com>
2025-04-12 05:32:00 +0100

    route for search result

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -12,6 +12,7 @@ import Login from './pages/Auth/Login'
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import PropertyDetail from './pages/PropertyDetail';
 import Shortlet from './pages/Shortlet';
+import SearchResults from './pages/SearchResults';
 
 import LayoutWrapper from './layouts/LayoutWrapper';
 
@@ -42,9 +43,10 @@ import ProfilePage from './pages/ProfilePage';
 
 import AdminDashboardOverview from './pages/admin/AdminDashboardOverview';
 import ManageUsers from './pages/admin/ManageUsers';
-import ViewInquiries from './pages/admin/ViewInquiries';
 import ManageFeaturedListings from './pages/admin/ManageFeaturedListings';
 import ManageAgents from './pages/admin/ManageAgents';
+import AdminAllInquiries from './pages/admin/AdminAllInquiries';
+import AgentInquiries from './pages/agents/AgentInquiries';
 
 
 // Create a theme to disable hover styles
@@ -82,13 +84,14 @@ function App() {
             <Route path="/login" element={<Login />} />
             <Route path="/agents" element={<AgentList />} />
             <Route path="/signup" element={<Signup />} />
+            <Route path="/properties/search/results" element={<SearchResults />} />
 
             {/* Admin Routes */}
             <Route element={<PrivateRoute allowedRoles={["admin"]} />}>
               <Route path="/admin/dashboard" element={<AdminDashboardOverview />} />
               <Route path="/admin/manage-users" element={<ManageUsers />} />
               <Route path="/admin/manage-agents" element={<ManageAgents />} />
-              <Route path="/admin/manage-inquiries" element={<ViewInquiries />} />
+              <Route path="/admin/manage-inquiries" element={<AdminAllInquiries />} />
               <Route path="/admin/manage-listings" element={<ManageFeaturedListings />} />
             </Route>
 
@@ -99,6 +102,7 @@ function App() {
               <Route path="/agent/property/add" element={<AddProperty />} />
               <Route path="/agent/profile" element={<ProfilePage />} />
               <Route path="/agent/property/edit/:id" element={<EditProperty />} />
+              <Route path="/agent/messages" element={<AgentInquiries />} />
             </Route>
             
 
