a0ff672c82724c67b8307178d3cb5149d873dd0a
ndmichael <ukejemicheal@gmail.com>
2025-04-12 05:27:17 +0100

    search properties api in store

--- frontend/src/redux/store.js
+++ frontend/src/redux/store.js
@@ -1,10 +1,13 @@
 import { configureStore } from "@reduxjs/toolkit";
+import { searchPropertiesApi } from "./searchPropertiesApi";
 import authReducer from "./authSlice";
 import propertyReducer from "./propertySlice";
 import agentReducer from './agentSlice';
 import dashboardStat from './admin/dashboardSlice';
 import adminUsers from './admin/usersSlice';
 import adminAgents from "./admin/agentsSlice";
+import adminInquiries from "./admin/inquiriesSlice";
+import agentInquiries from "./inquiry/inquiriesSlice";
 
 
 const store = configureStore({
@@ -15,7 +18,13 @@ const store = configureStore({
     adminDashboardStat: dashboardStat,
     adminUsers: adminUsers,
     adminAgents: adminAgents,
+    inquiries: adminInquiries,
+    agentInquiries: agentInquiries,
+
+    [searchPropertiesApi.reducerPath]: searchPropertiesApi.reducer,
   },
+    middleware: (getDefaultMiddleware) =>
+      getDefaultMiddleware().concat(searchPropertiesApi.middleware),
 });
 
 export default store;
