44a6353a257ea586d5c55e6cbf9e3cea1afa64a5
ndmichael <ukejemicheal@gmail.com>
2025-08-14 14:43:12 +0100

    padding issues

--- frontend/src/components/HeroSection.jsx
+++ frontend/src/components/HeroSection.jsx
@@ -7,21 +7,27 @@ const HeroSection = () => {
       id="heroSection"
       sx={{
         color: "white",
-        p: 4,
+        p: 2,
         textAlign: "center",
       }}
     >
-      <Box py={7} sx={{maxWidth: 850, width: "100%"}}>
+      <Box  sx={{maxWidth: 850, width: "100%", mx: "auto"}}>
           {/* Hero Text */}
         <Typography 
           fontWeight="bold" 
           color="black" gutterBottom
-          variant="h2"
-            component="h1"
+          variant="h3"
+            component="h3"
             sx={{
               fontWeight: "bold",
               mb: 2,
               textShadow: "0 3px 6px rgba(0,0,0,0.4)",
+              fontSize: {
+                xs: "1.8rem", // phones
+                sm: "2.2rem", // tablets
+                md: "2.8rem", // small desktops
+                lg: "3.2rem", // large screens
+              },
             }}
         >
           Your Next Home Awaits
